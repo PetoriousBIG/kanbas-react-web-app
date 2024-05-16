173d7bc3ce9732a98e2f447b36c8e71e020eceb8
PetoriousBIG <peter.m.martin.1993@gmail.com>
2024-05-15 21:05:16 -0400

    fix: sweep for missing code

--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -13,8 +13,11 @@ export default function Kanbas() {
             <td valign="top">
               <Routes>
                 <Route path="/" element={<Navigate to="Dashboard" />} />
+                <Route path="Account" element={<h1>Account</h1>} />
                 <Route path="Dashboard" element={<Dashboard />} />
                 <Route path="Courses/:id/*" element={<Courses />} />
+                <Route path="Calendar" element={<h1>Calender</h1>} />
+                <Route path="Inbox" element={<h1>Inbox</h1>} />
               </Routes>
             </td>
           </tr>
