2fc786b81fe309b882f6528727068629e01294af
PetoriousBIG <peter.m.martin.1993@gmail.com>
2024-07-22 22:53:43 -0400

    a6

--- src/Kanbas/Courses/Navigation/index.tsx
+++ src/Kanbas/Courses/Navigation/index.tsx
@@ -2,7 +2,7 @@ import { Link, useLocation } from "react-router-dom";
 import "./index.css"
 export default function CoursesNavigation() {
     const { pathname } = useLocation()
-    const links = ["Home", "Modules", "Piazza", "Zoom", "Assignments", "Quizzes", "Grades"];
+    const links = ["Home", "Modules", "Piazza", "Zoom", "Assignments", "Quizzes", "Grades", "People"];
     return (
       <div id="wd-courses-navigation" className="list-group fs-5 rounded-0">
         {links.map((link) => (
